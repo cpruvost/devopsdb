760f3d070ae120235e450b657004805b0622bc1e
quizforparty44 <quizforparty44@gmail.com>
2018-10-02 16:25:38 +0200

    add patch script db update

--- PatchDb/patch.sh
+++ PatchDb/patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DBSYSTEMID=$(oci db system list --compartment-id="ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq" --query "data[?\"display-name\"=='MyTFDatabaseNode'].{Id:id}" | ./Jq/jq-linux64 .[0].Id)
-echo $DBSYSTEMID
+echo "DBSYSTEMID= $DBSYSTEMID"
