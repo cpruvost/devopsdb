7036c9e71c8e1653b73b732e5fbd694c59db100d
quizforparty44 <quizforparty44@gmail.com>
2018-10-02 16:42:14 +0200

    add patch script db update

--- PatchDb/patch.sh
+++ PatchDb/patch.sh
@@ -2,10 +2,10 @@
 export DBSYSTEMID=$(oci db system list --compartment-id="ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq" --query "data[?\"display-name\"=='MyTFDatabaseNode'].{Id:id}" | ./Jq/jq-linux64 .[0].Id)
 echo "DBSYSTEMID = $DBSYSTEMID"
 
-export DBID=$(oci db database list --db-system-id=$dbsystemid --compartment-id=ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq | jq .data[].id)
+export DBID=$(oci db database list --db-system-id=$dbsystemid --compartment-id=ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq | ./Jq/jq-linux64 .data[].id)
 echo "DBID = $DBID"
 
-export DBPATCHID=$(oci db patch list by-db-system --all --db-system-id=$dbsystemid | jq .data[].id)
+export DBPATCHID=$(oci db patch list by-db-system --all --db-system-id=$dbsystemid | ./Jq/jq-linux64 .data[].id)
 echo "DBPATCHID = $DBPATCHID)
 
 #Patch DBID
