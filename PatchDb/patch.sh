d6ad47c81b0ee9343c57a08efe86403aa284ff7c
quizforparty44 <quizforparty44@gmail.com>
2018-10-02 17:29:36 +0200

    add patch script db update

--- PatchDb/patch.sh
+++ PatchDb/patch.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 export DBSYSTEMID=$(oci db system list --compartment-id="ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq" --query "data[?\"display-name\"=='MyTFDatabaseNode'].{Id:id}" | ./Jq/jq-linux64 .[0].Id)
+DBSYSTEMID=${DBSYSTEMID#'"'}
+DBSYSTEMID=${DBSYSTEMID::-1}
 echo "DBSYSTEMID = $DBSYSTEMID"
 
-export DBID=$(oci db database list --db-system-id=$dbsystemid --compartment-id=ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq | ./Jq/jq-linux64 .data[].id)
+export DBID=$(oci db database list --db-system-id=$DBSYSTEMID --compartment-id=ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq --query "data[?\"display-name\"=='MyTFDatabaseNode'].{Id:id}" | ./Jq/jq-linux64 .data[].id)
+DBID=${DBID#'"'}
+DBID=${DBID::-1}
 echo "DBID = $DBID"
 
-export DBPATCHID=$(oci db patch list by-db-system --all --db-system-id=$dbsystemid | ./Jq/jq-linux64 .data[].id)
+export DBPATCHID=$(oci db patch list by-db-system --all --db-system-id=$DBSYSTEMID | ./Jq/jq-linux64 .data[].id)
+DBPATCHID=${DBPATCHID#'"'}
+DBPATCHID=${DBPATCHID::-1}
 echo "DBPATCHID = $DBPATCHID)
 
 #Patch DBID
-#oci db database patch --database-id=$dbid --patch-action="APPLY" --patch-id=$dbpatchid
+#oci db database patch --database-id=$DBID --patch-action="APPLY" --patch-id=$DBPATCHID
 
 #Backup after Patch
-#oci db backup create --database-id=$dbid --display-name="backup before patch 18.3" --wait-for-state="ACTIVE"
+#oci db backup create --database-id=$DBID --display-name="backup before patch 18.3" --wait-for-state="ACTIVE"
