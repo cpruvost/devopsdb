9b9b247638dad67cf8cafa48e7e9d49a12b1507c
quizforparty44 <quizforparty44@gmail.com>
2018-10-04 10:12:27 +0200

    add sql create schema

--- Schema/create_schema.sql
+++ Schema/create_schema.sql
@@ -9,29 +9,24 @@ BEGIN
     WHERE  USERNAME = 'FLYWAY';
 
     IF cnt = 1 THEN
-        BEGIN
-            dbms_output.Put_line('User Flyway Already Exists');   
-        END;
+        dbms_output.Put_line('User Flyway Already Exists');   
     ELSE
-        BEGIN
-		    CREATE USER quizflyway IDENTIFIED BY quizflyway CONTAINER=CURRENT;
-			grant connect, resource to quizflyway;
-			alter user quizflyway quota unlimited on users;
-			alter user quizflyway quota unlimited on system;
-			grant create view to quizflyway;
-			alter user quizflyway enable editions;
-			GRANT CREATE ANY EDITION, DROP ANY EDITION to quizflyway;
-			CREATE EDITION V1;
-			CREATE EDITION V1POINT1;
-			CREATE EDITION V1POINT2;
-			grant use on edition V1 to quizflyway;
-			grant use on edition V1POINT1 to quizflyway;
-			grant use on edition V1POINT2 to quizflyway;
-			
-			dbms_output.Put_line('User Flyway OK');
-        END;   
+		EXECUTE IMMEDIATE 'CREATE USER quizflyway IDENTIFIED BY quizflyway CONTAINER=CURRENT';
+		EXECUTE IMMEDIATE 'grant connect, resource to quizflyway';
+		EXECUTE IMMEDIATE 'alter user quizflyway quota unlimited on users';
+		EXECUTE IMMEDIATE 'alter user quizflyway quota unlimited on system';
+		EXECUTE IMMEDIATE 'grant create view to quizflyway';
+		EXECUTE IMMEDIATE 'alter user quizflyway enable editions';
+		EXECUTE IMMEDIATE 'GRANT CREATE ANY EDITION, DROP ANY EDITION to quizflyway';
+		EXECUTE IMMEDIATE 'CREATE EDITION V1';
+		EXECUTE IMMEDIATE 'CREATE EDITION V1POINT1';
+		EXECUTE IMMEDIATE 'CREATE EDITION V1POINT2';
+		EXECUTE IMMEDIATE 'grant use on edition V1 to quizflyway';
+		EXECUTE IMMEDIATE 'grant use on edition V1POINT1 to quizflyway';
+		EXECUTE IMMEDIATE 'grant use on edition V1POINT2 to quizflyway';
+		
+		dbms_output.Put_line('User Flyway OK');   
     END IF;
- 
 END;
 /
 
