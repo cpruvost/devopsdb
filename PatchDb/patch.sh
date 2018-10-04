0f068d4ac06a5de8812e19a8e778f79349e14f68
quizforparty44 <quizforparty44@gmail.com>
2018-10-04 14:26:02 +0200

    add patch script db update

--- PatchDb/patch.sh
+++ PatchDb/patch.sh
@@ -12,7 +12,7 @@ echo "DBID = $DBID"
 #export DBPATCHID=$(oci db patch list by-db-system --all --db-system-id=$DBSYSTEMID | ./Jq/jq-linux64 .data[].id)
 #DBPATCHID=${DBPATCHID#'"'}
 #DBPATCHID=${DBPATCHID::-1}
-#echo "DBPATCHID = $DBPATCHID)
+#echo "DBPATCHID = $DBPATCHID"
 
 #Patch DBID
 #oci db database patch --database-id=$DBID --patch-action="APPLY" --patch-id=$DBPATCHID
@@ -24,13 +24,14 @@ echo "DBID = $DBID"
 export DBDOMAIN=$(oci db system list --compartment-id="ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq" --query "data[?\"display-name\"=='MyTFDatabaseNode'].{Domain:domain}" | ./Jq/jq-linux64 .[0].Domain)
 DBDOMAIN=${DBDOMAIN#'"'}
 DBDOMAIN=${DBDOMAIN::-1}
-echo $DBDOMAIN
+echo "DBDOMAIN = $DBDOMAIN"
 export DBUNNAME=$(oci db database list --db-system-id=$DBSYSTEMID --compartment-id=ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq --query "data[?\"db-name\"=='aTFdb'].{DbUnName:\"db-unique-name\"}" | ./Jq/jq-linux64 .[0].DbUnName)
 DBUNNAME=${DBUNNAME#'"'}
 DBUNNAME=${DBUNNAME::-1}
-echo $DBUNNAME
+echo "DBUNNAME = $DBUNNAME"
 DBCONSTRING=$DBUNNAME"."$DBDOMAIN
-echo $DBCONSTRING
+echo "DBCONSTRING = $DBCONSTRING"
+rm -f /tmp/outputDBCONSTRING.txt
 echo $DBCONSTRING >> /tmp/outputDBCONSTRING.txt
 
 
