e8fc62aa99e6b57f4d8a8bc7010937a674082f15
quizforparty44 <quizforparty44@gmail.com>
2018-10-04 09:58:39 +0200

    add patch script db update

--- PatchDb/patch.sh
+++ PatchDb/patch.sh
@@ -19,3 +19,18 @@ echo "DBID = $DBID"
 
 #Backup after Patch
 #oci db backup create --database-id=$DBID --display-name="backup before patch 18.3" --wait-for-state="ACTIVE"
+
+#Get Database Service Name
+export DBDOMAIN=$(oci db system list --compartment-id="ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq" --query "data[?\"display-name\"=='MyTFDatabaseNode'].{Domain:domain}" | ./Jq/jq-linux64 .[0].Domain)
+DBDOMAIN=${DBDOMAIN#'"'}
+DBDOMAIN=${DBDOMAIN::-1}
+echo $DBDOMAIN
+export DBUNNAME=$(oci db database list --db-system-id=$DBSYSTEMID --compartment-id=ocid1.compartment.oc1..aaaaaaaach4c3i5kpecjfjqwp3wfjfmbbjniyzieaxhcjs6c73ya2wl6vaxq --query "data[?\"db-name\"=='aTFdb'].{DbUnName:\"db-unique-name\"}" | ./Jq/jq-linux64 .[0].DbUnName)
+DBUNNAME=${DBUNNAME#'"'}
+DBUNNAME=${DBUNNAME::-1}
+echo $DBUNNAME
+DBCONSTRING=$DBUNNAME"."$DBDOMAIN
+echo $DBCONSTRING
+echo $DBCONSTRING >> /tmp/outputDBCONSTRING.txt
+
+
